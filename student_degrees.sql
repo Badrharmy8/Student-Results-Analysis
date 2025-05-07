3f23cf99c962401a6b5d4c8325d766b47dc0f0e3
badr <130597977+Badrharmy8@users.noreply.github.com>
2025-05-07 21:34:18 +0300

    Add SQL script containing queries for: - Top 10 students by grade - Success and failure rates (overall and by category flag)

---
+++ student_degrees.sql
@@ -0,0 +1,41 @@
+﻿use Students_degrees;
+
+-- جميع البيانات
+select * from S_Data;
+
+-- العشره الاوائل
+select top 10  الاسم , round(الدرجة , 2)
+from S_Data
+order by الدرجة desc
+
+-- عدد طلاب الناجحين والراسبين
+select student_case_desc, count(رقم_الجلوس)
+from S_Data
+group by student_case_desc 
+
+-- متوسط درجه النجاح و الرسوب
+select student_case_desc, round(avg(الدرجة) , 2) 
+from S_Data
+group by student_case_desc
+order by round(avg(الدرجة) , 2) desc
+
+-- عدد الطلاب الناجحين دور اول في الانتظام والانتساب والتحويل
+select c_flage ,count(رقم_الجلوس)
+from S_Data
+where student_case = 1
+group by c_flage
+
+-- عدد الطلاب الراسبين في الانتظام والانتساب والتحويل
+select c_flage ,count(رقم_الجلوس)
+from S_Data
+where student_case = 3
+group by c_flage
+
+
+-- عدد الطلاب الانتظام والانتساب والتحويل
+select c_flage , count(رقم_الجلوس)
+from S_Data
+group by c_flage
+
+
+
